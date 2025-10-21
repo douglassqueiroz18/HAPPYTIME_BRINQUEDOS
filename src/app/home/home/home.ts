e11cfb70e76938d2e81dcbc8e5c36d3d8cc10b68
douglassqueiroz18 <douglas.queiroz25@outlook.com>
2025-10-20 22:49:27 -0300

    testando push

--- src/app/home/home/home.ts
+++ src/app/home/home/home.ts
@@ -21,7 +21,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 })
 export class HomeComponent {
   logoState = 'normal';
-  letters = 'HAPPYTIME'.split('');
+  letters = 'HAPPYTIMEEE'.split('');
   isSmallScreen = false;
 
   colors = ['#ff4c4c', '#4c6cff', '#39FF14', '#ff8c00', '#9b59b6', '#ffd700'];
